c3249b009589549c2e06bf75b85a9171c93cd965
Stef <stefanspataro@hotmail.com>
2025-02-24 14:05:12 -0800

    movement relative to camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,13 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float speed;
-    
+    [SerializeField] private CinemachineCamera freeLookCamera;
+
+
     private Rigidbody rb;
     
     private void Start()
@@ -12,11 +15,23 @@ public class Player : MonoBehaviour
         inputManager.OnMove.AddListener(MovePlayer);
         rb = GetComponent<Rigidbody>();
     }
-   
+
+    private void Update()
+    {
+        transform.forward = freeLookCamera.transform.forward;
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+    }
 
     private void MovePlayer(Vector2 direction)
     {
-        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        Vector3 camForward = freeLookCamera.transform.forward;
+        Vector3 camRight = freeLookCamera.transform.right;
+        camForward.y = 0f;
+        camRight.y = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDirection = camForward * direction.y + camRight * direction.x;
         rb.AddForce(speed * moveDirection);
     }
 }
